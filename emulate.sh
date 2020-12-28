9d3ae503d92eea239daf7b6e0e7b3793b4db58ae
lbubuntu <lucamontechiesi@gmail.com>
2020-12-28 11:36:53 -0800

    add(makefiles): add makefile type build, ditch make.sh

--- emulate.sh
+++ emulate.sh
@@ -2,11 +2,12 @@
 
 # Emulate with qemu
 qemu-system-x86_64 \
-  -kernel 2_kernel/arch/x86_64/boot/bzImage \
-  -initrd 3_initramfs/initramfs.cpio.gz \
+  -kernel /home/crash/Documents/local/sysdig-repo/kernvirt/2_kernel/arch/x86_64/boot/bzImage \
+  -initrd 3_initramfs/build/initramfs.cpio.gz \
   -m 3048 \
-  -nographic \
-  -append "console=ttyS0 nokaslr" \
   -enable-kvm \
   -cpu host \
+  -append "console=ttyS0 nokaslr" \
+  -nographic \
 #  -s -S
+# -kernel 2_kernel/build/arch/x86_64/boot/bzImage \
