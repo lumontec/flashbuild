94a76e8c3745490a8f68c5acd61b8ff133da253b
lbubuntu <lucamontechiesi@gmail.com>
2020-12-28 15:03:23 -0800

    refactor(makefiles): cleaner management of patch inside dockerfiles

--- emulate.sh
+++ emulate.sh
@@ -3,11 +3,11 @@
 # Emulate with qemu
 qemu-system-x86_64 \
   -kernel /home/crash/Documents/local/sysdig-repo/kernvirt/2_kernel/arch/x86_64/boot/bzImage \
-  -initrd 3_initramfs/build/initramfs.cpio.gz \
+  -initrd 3_initramfs/initramfs.cpio.gz \
   -m 3048 \
   -enable-kvm \
   -cpu host \
   -append "console=ttyS0 nokaslr" \
   -nographic \
 #  -s -S
-# -kernel 2_kernel/build/arch/x86_64/boot/bzImage \
+#  -kernel 2_kernel/src/arch/x86_64/boot/bzImage \
