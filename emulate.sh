0715a7e9c61f0558df78ce7c6aa14f074e392e34
lbubuntu <lucamontechiesi@gmail.com>
2020-12-29 11:59:28 -0800

    add(makefile): multi architecture support with buildx

--- emulate.sh
+++ emulate.sh
@@ -2,7 +2,7 @@
 
 # Emulate with qemu
 qemu-system-x86_64 \
-  -kernel /home/crash/Documents/local/sysdig-repo/kernvirt/2_kernel/arch/x86_64/boot/bzImage \
+  -kernel 2_kernel/src/arch/x86_64/boot/bzImage \
   -initrd 3_initramfs/initramfs.cpio.gz \
   -m 3048 \
   -enable-kvm \
@@ -10,5 +10,4 @@ qemu-system-x86_64 \
   -append "console=ttyS0 nokaslr" \
   -nographic \
 #  -s -S
-#  -kernel 2_kernel/src/arch/x86_64/boot/bzImage \
-#  -kernel 2_kernel/src/arch/x86_64/boot/bzImage \
+#  -kernel /home/crash/Documents/local/sysdig-repo/kernvirt/2_kernel/arch/x86_64/boot/bzImage \
