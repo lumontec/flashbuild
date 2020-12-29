edd52241f04c2b6ac759d5bc4ddc64ece882edf0
lbubuntu <lucamontechiesi@gmail.com>
2020-12-29 01:49:50 -0800

    add(examples): add example folder

--- emulate.sh
+++ emulate.sh
@@ -2,7 +2,7 @@
 
 # Emulate with qemu
 qemu-system-x86_64 \
-  -kernel /home/crash/Documents/local/sysdig-repo/kernvirt/2_kernel/arch/x86_64/boot/bzImage \
+  -kernel 2_kernel/src/arch/x86_64/boot/bzImage \
   -initrd 3_initramfs/initramfs.cpio.gz \
   -m 3048 \
   -enable-kvm \
@@ -11,3 +11,4 @@ qemu-system-x86_64 \
   -nographic \
 #  -s -S
 #  -kernel 2_kernel/src/arch/x86_64/boot/bzImage \
+#  -kernel /home/crash/Documents/local/sysdig-repo/kernvirt/2_kernel/arch/x86_64/boot/bzImage \
