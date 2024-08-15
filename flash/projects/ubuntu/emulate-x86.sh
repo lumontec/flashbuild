8cadd24922c0998843d4bee81286a9d996d321b2
lmontech <luca.montechiesi@siemens.com>
2024-08-15 16:43:30 -0700

    Update Makefile,qemu: fix flags

--- flash/projects/ubuntu/emulate-x86.sh
+++ flash/projects/ubuntu/emulate-x86.sh
@@ -2,10 +2,13 @@
 
 # Emulate with qemu
 qemu-system-x86_64  \
-  -initrd ./initramfs.cpio \
+  -initrd ./initramfs.cpio.gz \
   -kernel ./vmlinuz \
   -m 2048 \
   -append "console=ttyS0 nokaslr" \
   -nographic \
   -cpu host \
+  -net nic,model=virtio \
+  -L /usr/share/seavgabios \
   -enable-kvm 
+
